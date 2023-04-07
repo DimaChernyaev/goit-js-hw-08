ad3fda1044001a304deb6de0060a2a74c8bc499e
Dima_ <chernyaev603@gmail.com>
2023-04-08 00:21:34 +0300

    complete home work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_VIDEO_TIME = 'videoplayer-current-time';
+let currentVideoTime = localStorage.getItem(KEY_VIDEO_TIME) ?? '0';
+// console.log(currentVideoTime);
+
+const iframe = document.querySelector('iframe');
+
+console.log(iframe);
+
+const player = new Player(iframe);
+
+
+function onPlay() {
+    player.getCurrentTime().then(function(seconds) {
+        currentVideoTime = seconds;
+        localStorage.setItem(KEY_VIDEO_TIME, currentVideoTime);
+    });
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(currentVideoTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
+
+const USER_INFO_KEY = "feedback-form-state";
+
+const formEl = document.querySelector('.feedback-form');
+const email = formEl.email;
+const message = formEl.message;
+
+formEl.addEventListener('submit', submitUserInfo);
+email.addEventListener('input', throttle(savedUserInfo, 500));
+message.addEventListener('input', throttle(savedUserInfo, 500));
+
+
+function submitUserInfo(event) {
+    event.preventDefault();
+
+    event.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(USER_INFO_KEY)));
+    localStorage.removeItem(USER_INFO_KEY);
+}
+
+function savedUserInfo() {
+    const userInfo = {
+        email:  email.value,
+        message: message.value,
+    };
+
+    localStorage.setItem(USER_INFO_KEY, JSON.stringify(userInfo));
+} 
+
+
+function demonstrateSavedUserInfo() {
+
+    const haveUserKey = localStorage.getItem(USER_INFO_KEY);
+
+    if (haveUserKey) {
+        const sevedUserInfoObj = JSON.parse(localStorage.getItem(USER_INFO_KEY));
+        email.value = sevedUserInfoObj.email;
+        message.value = sevedUserInfoObj.message;
+    }
+}
+
+demonstrateSavedUserInfo()
+
+
 
